c60bb4dc4b1e8986f2e316194d0c86e4dde8c83f
adrianjcorrea <adrianjcorrea007@gmail.com>
2019-09-06 18:17:36 -0500

    Created QRCode generator project using VB.

---
+++ QRCodeApp/QRCodeApp/Form1.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Pic = New System.Windows.Forms.PictureBox()
+        CType(Me.Pic, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.Location = New System.Drawing.Point(347, 415)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(75, 23)
+        Me.btnGenerate.TabIndex = 0
+        Me.btnGenerate.Text = "Generate"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(57, 309)
+        Me.txtInput.MaximumSize = New System.Drawing.Size(500, 500)
+        Me.txtInput.Multiline = True
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(365, 100)
+        Me.txtInput.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 312)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Input:"
+        '
+        'Pic
+        '
+        Me.Pic.Location = New System.Drawing.Point(15, 12)
+        Me.Pic.Name = "Pic"
+        Me.Pic.Size = New System.Drawing.Size(407, 274)
+        Me.Pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.Pic.TabIndex = 3
+        Me.Pic.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(434, 450)
+        Me.Controls.Add(Me.Pic)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.btnGenerate)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "QR Code"
+        CType(Me.Pic, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnGenerate As Button
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Pic As PictureBox
+End Class
---
+++ QRCodeApp/QRCodeApp/Form1.vb
@@ -0,0 +1,11 @@
+﻿Imports QRCoder
+
+Public Class Form1
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        Dim gen As New QRCodeGenerator
+        Dim r As String = "hello"
+        Dim data = gen.CreateQrCode(r, QRCodeGenerator.ECCLevel.Q)
+        Dim code As New QRCode(data)
+        Pic.Image = code.GetGraphic(6)
+    End Sub
+End Class
